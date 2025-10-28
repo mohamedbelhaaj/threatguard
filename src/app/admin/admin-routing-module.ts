7209ff01d82a7c87679e59dd9b6cf0121a1e2d8e
mohamedbelhaaj <mohamedbelhajka@gmail.com>
2025-10-28 09:50:23 +0100

    v3

--- src/app/admin/admin-routing-module.ts
+++ src/app/admin/admin-routing-module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { IpBlocking } from './ip-blocking/ip-blocking';
+import { ServerManagement } from './server-management/server-management';
+import { TaskManagement } from './task-management/task-management';
+import { AdminChatbot } from './admin-chatbot/admin-chatbot';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard', component:Dashboard},
+  {path:'ip-bloxking', component:IpBlocking},
+  {path:'server-managment', component:ServerManagement},
+  {path:'task-managment', component:TaskManagement},
+  {path:'chatboot' , component:AdminChatbot}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/soc-analyst/soc-analyst-routing-module.ts
+++ src/app/soc-analyst/soc-analyst-routing-module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Chatbot } from './chatbot/chatbot';
+import { Alerts } from './alerts/alerts';
+import { Dashboard } from './dashboard/dashboard';
+import { ThreatScanner } from './threat-scanner/threat-scanner';
+import { ThreatsDetected } from './threats-detected/threats-detected';
+import { Reports } from './reports/reports';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'reports' , component:Chatbot},
+{path:'chatbot', component:Alerts},
+{path:'dashboard' , component:Dashboard},
+{path:'threat-scanner', component:ThreatScanner},
+{path:'threat-detected', component:ThreatsDetected},
+{path:'report', component:Reports}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
